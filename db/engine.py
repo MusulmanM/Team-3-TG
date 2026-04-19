039d94ce435e335344850702336540ea65f52237
musulman <mmmmmmvvv200@gmail.com>
2026-04-19 21:06:10 +0500

    Update engine.py

--- db/engine.py
+++ db/engine.py
@@ -1,5 +1,17 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from .models import Base
 
-engine = create_async_engine("sqlite+aiosqlite:///./bot.db")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+else:
+
+    DATABASE_URL = "sqlite+aiosqlite:///./bot.db"
+
+
+engine = create_async_engine(DATABASE_URL)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
